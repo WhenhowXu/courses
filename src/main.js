1554b4af49710beff4bfa9a2ba5157ffbbf20e75
xuwenhao <673438390@qq.com>
2023-02-12 17:22:46 +0800

    fix: 注释掉不展示示例

--- src/main.js
+++ src/main.js
@@ -9,15 +9,14 @@ import plTable from "pl-table";
 import "pl-table/themes/index.css";
 import "pl-table/themes/plTableStyle.css";
 import VXETable from "vxe-table";
-import HaoUI from "@/components";
-import HaoComponentsVue from "@xuwenhao/hao-components-vue/lib/hao-ui.umd";
+import 'xe-utils'
+import 'vxe-table/lib/style.css'
 
 import "@/assets/icons";
 
 Vue.use(plTable);
 Vue.use(VXETable);
-Vue.use(HaoComponentsVue);
-Vue.use(directives).use(Antd).use(HaoUI);
+Vue.use(directives).use(Antd);
 
 Vue.config.productionTip = false;
 
